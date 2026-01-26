c84a15f276d4cefac1900c3172b490c6c77702fc
Ernesto <ehodoco2003@g.educaand.es>
2026-01-26 10:51:33 +0100

    casi terminado

--- funcs.py
+++ funcs.py
@@ -3,39 +3,102 @@ from datos import get_juegos, get_usuarios
 from datetime import datetime
 from Usuario import Usuario
 
-def login()-> tuple:
-    lista_usuarios=get_usuarios()
+def comprobar_usuario(nombre_usuario:str, lista_usuarios:list[object]) -> object :
+    for i,usuario_valido in enumerate(lista_usuarios):
 
-    for i in range(3):
-        nombre_usuario=input("Introduce tu nombre de usuario: ")
-        existe_usuario = False
-
-        for i,usuario_valido in enumerate(lista_usuarios):
-
-            if nombre_usuario == usuario_valido.nombre:
-                usuario = lista_usuarios[i]
-                existe_usuario = True
-                break
-       
-        if existe_usuario:
-            contrasenha=input("Introduce tu contraseña: ")
-
-            if contrasenha == usuario.contra:
-                return True, usuario
-            
-            else:
-                print(f"La contraseña es incorrecta\n")
-
-        else:
-            print(f"No existe ningun usuario llamado {nombre_usuario}\n")
-
-    print("No has conseguido autenticarte, cerrando programa...")
+        if nombre_usuario == usuario_valido.nombre:
+            usuario = lista_usuarios[i]
+            return usuario
 
 
-def print_menu() -> None:
+def comprobar_contrasenha(intento_contrasenha:str, usuario:object) -> bool:
+    if intento_contrasenha == usuario.contra:
+        return True
+    
+    else:
+        return False
+
+
+def login() -> object:
+    lista_usuarios = get_usuarios()
+    intentos = 0
+    existe_usuario = False
+    contrasenha_correcta = False
+
+    usuario=comprobar_usuario(input("Login: "), lista_usuarios)
+
+    while intentos < 2 and usuario == None:
+        intentos += 1
+        print("El nombre de usuario no existe")
+        usuario=comprobar_usuario(input("Inserte de nuevo el nombre: "), lista_usuarios)
+
+    if usuario != None:
+        existe_usuario = True
+
+    if existe_usuario:
+        contrasenha_correcta = comprobar_contrasenha(input("Inserte la contraseña: "), usuario)
+
+        while intentos < 2 and contrasenha_correcta == False:
+            intentos += 1
+            contrasenha_correcta = comprobar_contrasenha(input("Contraseña incorrecta, inserte de nuevo la contraseña: "), usuario)
+    
+    if contrasenha_correcta:
+        return usuario
+
+    if intentos >= 2:
+        print("No has conseguido identificarte, cerrando programa...")
+        return None
+
+
+def juegos_aptos(usuario:object) -> list[object]:
     lista_juegos=get_juegos()
-    print("\nJUEGOS DISPONIBLES PARA COMPRAR:")
+    edad_usuario = usuario.edad()
+    lista_juegos_aptos = []
+
+    for juego in lista_juegos:
+        if edad_usuario >= juego.PEGI:
+            lista_juegos_aptos.append(juego)
+
+    return lista_juegos_aptos
+
+
+def print_menu(lista_juegos:list[object]) -> None:
+    print("JUEGOS DISPONIBLES PARA COMPRAR:")
+
     for i,juego in enumerate(lista_juegos):
-        print(f"[{i+1}]. {juego.nombre}")
-    print("--------------------------------")
+        print(f"[{i+1}]. {juego.nombre}, precio: {juego.precio_final(0, 0)}")
+    print("-------------------------------- \n")
+
+
+def ingresar_dinero() -> float:
+    while True:
+        try:
+            dinero = float(input("Cantidad a ingresar: "))
+        except:
+            print("Cantidad de dinero no válida, intentelo de nuevo")
+        else:
+            decimales_dinero_sobre2 = 100*(100*dinero-int(100*dinero))
+
+            if dinero < 0 or decimales_dinero_sobre2 != 0.0:
+                print("Cantidad de dinero no válida, intentelo de nuevo")
+            else:
+                return dinero
+
+
+def print_lista_juegos(lista_juegos:list[object]) -> None:
+    for juego in lista_juegos:
+            print(juego.nombre)
+
+
+def precio_carrito(carrito:list[object]) -> float:
+    precio_total = 0
+
+    for juego in carrito:
+        precio_juego = juego.precio_final(0, 0)
+        print(f"{juego.nombre} -- precio: {precio_juego}€")
+        precio_total += precio_juego
+
+    print(f"El precio total es {precio_total}")
+
+    return precio_total
 
--- steam.py
+++ steam.py
@@ -1,17 +1,70 @@
-from funcs import *
+from funcs import login, print_menu, ingresar_dinero, juegos_aptos, print_lista_juegos, precio_carrito
 
-datos_login = login()
-autenticado = datos_login[0]
-usuario = datos_login[1]
-del datos_login
+autenticado=False
+usuario = login()
+
+if usuario != None:
+    autenticado = True
+    lista_juegos = juegos_aptos(usuario)
+    juegos_usuario = []
+    carrito = []
+    print()
 
 while autenticado:
-    print_menu()
+    
+    print_menu(lista_juegos)
 
     print(f"Actualmente tienes {usuario.saldo}€")
+    print("--------------------------------")
+    print("[V]er mis juegos")
+    print("[I]ngresar dinero")
+    print("Ir al [C]arrito")
+    print("[S]alir")
 
-    input_usuario=input()
+    input_usuario=input("¿Que quieres hacer? ").lower()
+    print("--------------------------------")
 
-    match input_usuario:
-        case 1:
-            die
+    if input_usuario == "v":
+        print("Tienes los siguientes juegos:")
+        print_lista_juegos(juegos_usuario)
+
+    elif input_usuario == "i":
+        dinero_ingresar = ingresar_dinero()
+        print(f"Enhorabuena has ingresado {dinero_ingresar}€")
+        usuario.saldo += dinero_ingresar
+
+    elif input_usuario == "c":
+        print("Tienes los siguientes juegos en el carrito:")
+        total_carrito = precio_carrito(carrito)
+        print("[P]agar")
+        print("[V]olver")
+        opcion_comprar = input().lower()
+
+        if opcion_comprar == "p":
+            if usuario.saldo >= total_carrito:
+                usuario.saldo -= total_carrito
+                juegos_usuario.extend(carrito)
+                carrito.clear()
+                print("Gracias por tu compra")
+            else:
+                print("Saldo insuficiente")
+            
+    elif input_usuario == "s":
+        break
+
+    else:
+        try:
+            int_input_usuario = int(input_usuario)-1
+            lista_juegos[int_input_usuario]
+        except:
+            print("\n Opción no válida")
+        else:
+            print(f"\n{lista_juegos[int_input_usuario]}")
+            opcion_anhadir_carrito = input("¿Quieres añadir el juego al carrito? (S/N) ").lower()
+
+            if opcion_anhadir_carrito == "s":
+                carrito.append(lista_juegos[int_input_usuario])
+                lista_juegos.pop(int_input_usuario)
+    
+    input()
+
